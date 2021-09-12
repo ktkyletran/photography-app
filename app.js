722ade78994c7be6cab8ed01e44c11779378318b
Vanity <ktkyletran@gmail.com>
2021-09-12 16:59:41 -0500

    added gsap animations and scroll trigger

--- app.js
+++ app.js
@@ -11,3 +11,30 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+// Animations
+gsap.registerPlugin(ScrollTrigger);
+var tl = gsap.timeline();
+
+tl.from('.fx1', {
+  duration: 0.6,
+  opacity: 0,
+  y: 150,
+  stagger: 0.3,
+});
+
+tl.from('.fx3', {
+  duration: 0.4,
+  opacity: 0,
+});
+
+// tl.from('.fx2', {
+//   ScrollTrigger: {
+//     trigger: '.fx2',
+//     start: 'top top',
+//   },
+//   duration: 0.6,
+//   opacity: 0,
+//   x: -50,
+//   stagger: 0.2,
+// })
