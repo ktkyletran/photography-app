6b927064cae60e8edcc950aeda6eb9b7de857804
Vanity <ktkyletran@gmail.com>
2021-09-12 18:39:33 -0500

    gsap animations

--- app.js
+++ app.js
@@ -14,27 +14,76 @@ menu.addEventListener('click', mobileMenu);
 
 // Animations
 gsap.registerPlugin(ScrollTrigger);
-var tl = gsap.timeline();
 
-tl.from('.fx1', {
+gsap.from('.fx1', {
   duration: 0.6,
   opacity: 0,
-  y: 150,
-  stagger: 0.3,
+  x: 35,
 });
-
-tl.from('.fx3', {
-  duration: 0.4,
+gsap.from('.fx2', {
+  duration: 0.6,
   opacity: 0,
+  x: -35,
 });
 
-// tl.from('.fx2', {
-//   ScrollTrigger: {
-//     trigger: '.fx2',
-//     start: 'top top',
-//   },
-//   duration: 0.6,
-//   opacity: 0,
-//   x: -50,
-//   stagger: 0.2,
-// })
+gsap.from('.fx3', {
+  duration: 0.4,
+  y: 35,
+  opacity: 0,
+  delay: 0.8,
+});
+
+gsap.from('.fx4', {
+  scrollTrigger: {
+    trigger: '.fx4',
+    start: "top center",
+  },
+  opacity: 0,
+  x: 50,
+  duration: 0.8,
+  stagger: 0.2,
+});
+
+gsap.from('.fx5', {
+  scrollTrigger: {
+    trigger: '.fx5',
+    start: "top bottom"
+  },
+  opacity: 0,
+  y: 30,
+  duration: 0.8,
+  stagger: 0.2,
+});
+
+gsap.from('.fx6', {
+  scrollTrigger: {
+    trigger: '.fx6',
+    start: "top center"
+  },
+  opacity: 0,
+  y: 30,
+  duration: 0.8,
+  stagger: 0.4,
+});
+
+gsap.from('.fx7', {
+  scrollTrigger: {
+    trigger: '.fx7',
+    start: "top center"
+  },
+  opacity: 0,
+  y: 40,
+  duration: 0.8,
+  stagger: 0.2,
+});
+
+gsap.from('.fx8', {
+  scrollTrigger: {
+    trigger: '.fx8',
+    start: "top center"
+  },
+  opacity: 0,
+  duration: 0.8,
+  stagger: 0.6,
+  delay: 1.2
+});
